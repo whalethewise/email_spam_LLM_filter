7c36a767701508079f0f27f079a9ed54e4df8190
whalethewise <whalethewise@gmail.com>
2026-04-01 14:31:33 -0400

    Moved version to gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-val springAiVersion = "1.0.0"
-
 plugins {
     java
     id("org.springframework.boot") version "3.5.13"
@@ -8,7 +6,6 @@ plugins {
 }
 
 group = "ca.aksentiev"
-version = "0.0.1-SNAPSHOT"
 description = "AIEmailProcessingPlatform"
 
 java {
@@ -22,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.springframework.ai:spring-ai-bom:${springAiVersion}"))
+    implementation(platform("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}"))
     implementation("org.springframework.ai:spring-ai-starter-model-ollama")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-mail")
